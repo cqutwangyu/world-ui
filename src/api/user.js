dbec7b4c7cb89b590e6693187894c38540e42729
cqutwangyu <cqwangyu@2016.cqut.edu.cn>
2022-08-29 15:26:38 +0800

    feat(登录):oauth2鉴权

--- src/api/user.js
+++ src/api/user.js
@@ -18,7 +18,7 @@ export function register(data) {
 
 export function getInfo() {
     return request({
-        url: '/admin/info',
+        url: '/world-admin/admin/info',
         method: 'get'
     })
 }
